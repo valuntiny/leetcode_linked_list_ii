d4e8235c15b9a1ec7e444821c5249596c59648db
valuntiny <459201296@qq.com>
2019-01-03 20:56:36 -0500

    doing second traversal

---
+++ getIntersectionNode.py
@@ -0,0 +1,27 @@
+'''
+Write a program to find the node at which the intersection of two singly linked lists begins.
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if not headA or not headB:
+            return None
+
+        pa = headA
+        pb = headB
+        while pa != pb:
+            pa = pa.next if pa else headB
+            pb = pb.next if pb else headA
+
+        return pa
---
+++ test.py
@@ -0,0 +1,28 @@
+'''
+to test the function of 'is' and 'is not'
+'''
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+a = ListNode(1)
+b = ListNode(1)
+c = a
+d = None
+
+if a is b:
+    print "T"
+else:
+    print "F"
+
+if a is c:
+    print "T"
+else:
+    print "F"
+
+if d:
+    print "T"
+else:
+    print "F"
